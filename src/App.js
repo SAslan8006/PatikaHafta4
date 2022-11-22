6a56b8855d4e7a7b1498fc7581687c33a8f6afc5
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2022-11-22 21:51:16 +0300

    styles main

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import FlashMessage from 'react-native-flash-message';
 import { Navigation } from '~/navigation';
-import stles from '~/utils/styles';
+import stles from '~/themes/main.app.styles';
 import store from '~/redux';
 
 const App = props => {
