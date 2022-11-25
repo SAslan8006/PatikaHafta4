30ca205ce6c950a9e8f1bac666889e4068a96915
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2022-11-25 22:08:15 +0300

    Product

--- src/screens/Products/Products.js
+++ src/screens/Products/Products.js
@@ -5,12 +5,11 @@ import Loading from '~/components/Loading';
 
 const Products = ({ route }) => {
     const category = route.params.category;
-    console.log(category);
 
     const { data, loading } = useFetch("https://dummyjson.com/products/category/" + category);
 
     const renderProducts = ({ item }) => <ProductCards products={item} />
-
+    
     if (data.length == 0) {
         return <Loading />
     }
