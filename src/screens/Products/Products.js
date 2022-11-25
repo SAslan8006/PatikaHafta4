bd68c9f6ab4b4e49db1f3f69d2b0578790ee1e11
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2022-11-25 22:08:15 +0300

    Product

--- src/screens/Products/Products.js
+++ src/screens/Products/Products.js
@@ -8,11 +8,13 @@ const Products = ({ route, navigation }) => {
 
     const { data, loading } = useFetch("https://dummyjson.com/products/category/" + category);
 
+
     const renderProducts = ({ item }) => <ProductCards products={item} onPress={() => handleOnPress(item)} />
 
     const handleOnPress = (product) => {
         navigation.navigate("Details", product);
     }
+
 
     if (data.length == 0) {
         return <Loading />
